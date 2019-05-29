8f361eede14d1b085379dbed4b0f77f313add89c
Rafaelbap <rbbrox@gmail.com>
2019-05-29 14:22:14 +0000

    adicionou view para criar/editar os post do blog dentro do site. 29.05.19 V1

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from .models import Post
 from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
 def post_list(request):# cria função post_list que recebe um request
-    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts}) #executa render que rendeiza/monta o modelo de acordo com o template blog/po_list.html. Retorna (return) o resultado.
+                                                                    #A parte antes dos dois pontos ('post':post) é uma string, por isso está entre "".
