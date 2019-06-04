34378830ccc71a7be3c02ba44317af33720c6dfe
Rafaelbap <rbbrox@gmail.com>
2019-06-04 20:07:14 +0000

    atualização do visual e template para detalhes do post 04.06.19 17:07

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,3 +9,11 @@ def post_list(request):# cria função post_list que recebe um request
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts}) #executa render que rendeiza/monta o modelo de acordo com o template blog/po_list.html. Retorna (return) o resultado.
                                                                     #A parte antes dos dois pontos ('post':post) é uma string, por isso está entre "".
+
+
+
+
+def post_detail(request, pk):
+    Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
